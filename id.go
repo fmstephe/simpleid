7f788c1c399e0429205d166586d27b6ed560f22a
Francis Stephens <francisstephens@gmail.com>
2011-11-12 23:49:28 +0000

    Changed id.go so it was usable by external packages.

--- id.go
+++ id.go
@@ -1,15 +1,19 @@
-package locserver
+package id
 
 import (
 	"sync"
 )
 
-type idMaker struct {
+type IdMaker struct {
 	id   int64
 	lock sync.Mutex
 }
 
-func (i *idMaker) new() int64 {
+func New() *IdMaker {
+	return new(IdMaker)
+}
+
+func (i *IdMaker) NewId() int64 {
 	i.lock.Lock()
 	defer i.lock.Unlock()
 	i.id++
